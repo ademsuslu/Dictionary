1b774b86f552e4eec58fa780daa6406507c81547
ademsuslu <ademsuslu9080@gmail.com>
2023-01-14 20:48:51 +0300

    refactor: icons

---
+++ src/Components/Icons/IconsBar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { AiOutlineHeart } from "react-icons/ai";
+import { BsBookmark, BsShare } from "react-icons/bs";
+import { BiMessageRounded } from "react-icons/bi";
+import { useRouter } from "next/router";
+import { toast } from "react-hot-toast";
+
+export default function IconsBar({ whiteBackground }) {
+  const { asPath } = useRouter();
+  const origin =
+    typeof window !== "undefined" && window.location.origin
+      ? window.location.origin
+      : "";
+
+  const URL = `${origin}${asPath}`;
+
+  function copyToClipboard() {
+    navigator.clipboard.writeText(URL);
+    toast.success("Page url copied successufully!");
+  }
+
+  return (
+    <ul className="IconList">
+      <li className={`IconListItem ${whiteBackground && "IconListItemWhite"}`}>
+        <AiOutlineHeart className="IconListItemIcon" />
+      </li>
+      <li className={`IconListItem ${whiteBackground && "IconListItemWhite"}`}>
+        <BsBookmark className="IconListItemIcon" />
+      </li>
+      <li className={`IconListItem ${whiteBackground && "IconListItemWhite"}`}>
+        <BiMessageRounded className="IconListItemIcon" />
+      </li>
+      <li
+        className={`IconListItem ${whiteBackground && "IconListItemWhite"}`}
+        onClick={copyToClipboard}
+      >
+        <BsShare className="IconListItemIcon" />
+      </li>
+    </ul>
+  );
+}
--- src/pages/[slug].js
+++ src/pages/[slug].js
@@ -1,17 +1,25 @@
+import IconsBar from "@/Components/Icons/IconsBar";
 import DictionaryData from "../Data/dictionary";
 
 const Details = ({ data }) => {
   return (
-    <div>
-      {data?.name}
-
-      <script
-        async
-        src="https://comments.app/js/widget.js?3"
-        data-component-app-website="8lLlUBuH"
-        data-limit="5"
-      />
-    </div>
+    <>
+      <div className="Detay">
+        <h1 className="DetayHead">{data?.name}</h1>
+        <script
+          async
+          src="https://comments.app/js/widget.js?3"
+          data-component-app-website="8lLlUBuH"
+          data-limit="5"
+        />
+      </div>
+      <div>
+        <p>{data?.description}</p>
+      </div>
+      <div>
+        <IconsBar whiteBackground />
+      </div>
+    </>
   );
 };
 
