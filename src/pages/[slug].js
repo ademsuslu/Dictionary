865a4d7cd351c53dc0c50a5d2136255452b43968
ademsuslu <ademsuslu9080@gmail.com>
2023-01-15 15:21:24 +0300

    style: comments div

--- src/pages/[slug].js
+++ src/pages/[slug].js
@@ -6,18 +6,21 @@ const Details = ({ data }) => {
     <>
       <div className="Detay">
         <h1 className="DetayHead">{data?.name}</h1>
-        <script
-          async
-          src="https://comments.app/js/widget.js?3"
-          data-component-app-website="8lLlUBuH"
-          data-limit="5"
-        />
       </div>
       <div>
         <p>{data?.description}</p>
       </div>
       <div className="DetayIconBar">
         <IconsBar whiteBackground />
+      </div>
+      <div id="comments">
+        <script
+          async
+          src="https://comments.app/js/widget.js?3"
+          data-comments-app-website="8lLlUBuH"
+          data-limit="5"
+          data-page-id="Home Page"
+        />
       </div>
     </>
   );
