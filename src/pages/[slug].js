d9bb008bc5301dc196c92638cc1ca4479c800193
ademsuslu <ademsuslu9080@gmail.com>
2023-01-15 16:48:18 +0300

    refactor:script Changed

--- src/pages/[slug].js
+++ src/pages/[slug].js
@@ -13,12 +13,14 @@ const Details = ({ data }) => {
       <div className="DetayIconBar">
         <IconsBar whiteBackground />
       </div>
-      <script
-        async
-        src="https://comments.app/js/widget.js?3"
-        data-comments-app-website="8lLlUBuH"
-        data-limit="5"
-      />
+      <div style={{ marginTop: "2rem", borderRadius: "16px" }}>
+        <script
+          async
+          src="https://comments.app/js/widget.js?3"
+          data-comments-app-website="8lLlUBuH"
+          data-limit="5"
+        />
+      </div>
     </>
   );
 };
