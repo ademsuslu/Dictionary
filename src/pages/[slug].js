2332553e819cd51daed232af01348a2db3a061a8
ademsuslu <ademsuslu9080@gmail.com>
2023-01-15 17:27:17 +0300

    commit

--- src/pages/[slug].js
+++ src/pages/[slug].js
@@ -1,9 +1,18 @@
 import IconsBar from "@/Components/Icons/IconsBar";
-import dynamic from "next/dynamic";
 import DictionaryData from "../Data/dictionary";
+import { useEffect } from "react";
 
 const Details = ({ data }) => {
   // const script = `<script async src="https://comments.app/js/widget.js?3" data-comments-app-website="8lLlUBuH" data-limit="5" />`;
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = "https://comments.app/js/widget.js?3";
+    script.setAttribute("data-comments-app-website", "8lLlUBuH");
+    script.setAttribute("data-limit", "5");
+    document.getElementById("comments-container").appendChild(script);
+  }, []);
 
   return (
     <>
@@ -16,6 +25,7 @@ const Details = ({ data }) => {
       <div className="DetayIconBar">
         <IconsBar whiteBackground />
       </div>
+      <div id="comments-container"></div>
       {/* <div
         style={{ marginTop: "2rem" }}
         dangerouslySetInnerHTML={{ __html: script }}
