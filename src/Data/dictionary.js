51cfa6c4a94a689e95ea1f965a9fc0ff532efce9
Adem suslu <84082893+ademsuslu@users.noreply.github.com>
2023-01-15 17:57:48 +0300

    Update dictionary.js

--- src/Data/dictionary.js
+++ src/Data/dictionary.js
@@ -23,6 +23,14 @@ const dictionary = [
     likeLink: "https://t.me/opendictionary1/2",
     saveLink: "https://t.me/opendictionary1/2",
   },
+    {
+    name: "Pineapple",
+    slug: "pineapple",
+    description:
+      "A round fruit with red or green skin and a firm white inside.",
+    likeLink: "https://t.me/opendictionary1/2",
+    saveLink: "https://t.me/opendictionary1/2",
+  },
 ];
 
 export default dictionary;
