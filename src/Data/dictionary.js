3b4d107f2f49c66622ecbb5bebf5f6255fa68b0a
ademsuslu <ademsuslu9080@gmail.com>
2023-01-16 15:06:19 +0300

    refactor: data

--- src/Data/dictionary.js
+++ src/Data/dictionary.js
@@ -1,35 +1,36 @@
 const dictionary = [
   {
     name: "Apple",
-    slug: "apple",
+    wordVersion: "1.1.3",
+    slug: "apple-1-1-3",
     description:
       "A round fruit with red or green skin and a firm white inside.",
     likeLink: "https://t.me/opendictionary1/2",
     saveLink: "https://t.me/opendictionary1/2",
+    version: "1.0",
+    md5: "123",
   },
   {
     name: "Banana",
+    wordVersion: "1.1.3",
     slug: "banana",
     description:
       "A round fruit with red or green skin and a firm white inside.",
     likeLink: "https://t.me/opendictionary1/2",
     saveLink: "https://t.me/opendictionary1/2",
+    version: "1.0",
+    md5: "1234",
   },
   {
     name: "Orange",
+    wordVersion: "1.1.0",
     slug: "orange",
     description:
       "A round fruit with red or green skin and a firm white inside.",
     likeLink: "https://t.me/opendictionary1/2",
     saveLink: "https://t.me/opendictionary1/2",
-  },
-    {
-    name: "Pineapple",
-    slug: "pineapple",
-    description:
-      "A round fruit with red or green skin and a firm white inside.",
-    likeLink: "https://t.me/opendictionary1/2",
-    saveLink: "https://t.me/opendictionary1/2",
+    version: "1.0",
+    md5: "12345",
   },
 ];
 
