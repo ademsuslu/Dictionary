365be23a921a3677553766634b9edac223c18dd8
ademsuslu <ademsuslu9080@gmail.com>
2023-01-16 19:16:19 +0300

    refactor: data

--- src/Data/dictionary.js
+++ src/Data/dictionary.js
@@ -11,6 +11,28 @@ const dictionary = [
     md5: "123",
   },
   {
+    name: "Apple",
+    wordVersion: "1.1.0",
+    slug: "apple-1-1-2",
+    description:
+      "A round fruit with red or green skin and a firm white inside.",
+    likeLink: "https://t.me/opendictionary1/2",
+    saveLink: "https://t.me/opendictionary1/2",
+    version: "1.0",
+    md5: "9879797",
+  },
+  {
+    name: "Apple",
+    wordVersion: "1.1.0",
+    slug: "apple-1-1-0",
+    description:
+      "A round fruit with red or green skin and a firm white inside.",
+    likeLink: "https://t.me/opendictionary1/2",
+    saveLink: "https://t.me/opendictionary1/2",
+    version: "1.0",
+    md5: "5464454",
+  },
+  {
     name: "Banana",
     wordVersion: "1.1.3",
     slug: "banana",
