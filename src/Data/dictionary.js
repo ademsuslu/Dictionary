203658327952f806ea4ed0b954738aea61b8900d
ademsuslu <ademsuslu9080@gmail.com>
2023-01-17 18:28:14 +0300

    refactor:add to Strawberry

--- src/Data/dictionary.js
+++ src/Data/dictionary.js
@@ -54,6 +54,17 @@ const dictionary = [
     telegramPost: "5",
     version: "1.0",
   },
+  {
+    name: "Strawberry",
+    wordVersion: "1.1.0",
+    slug: "strawberry-1-1-0",
+    description:
+      "A round fruit with red or green skin and a firm white inside.",
+    likeLink: "https://t.me/opendictionary1/2",
+    saveLink: "https://t.me/opendictionary1/2",
+    telegramPost: "6",
+    version: "1.0",
+  },
 ];
 
 export default dictionary;
