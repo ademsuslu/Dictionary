f01bf253d3edfe62762fa0e9a1485705c0693425
rknicker <rknicker88@gmail.com>
2023-01-22 17:07:14 -0700

    Update constants.js

--- src/Data/constants.js
+++ src/Data/constants.js
@@ -1,1 +1,2 @@
-export const telegramChannelName = "opendictionary1";
+export const telegramChannelName = "opendictionary1"; /*This is the telegram group name for the dictionary */
+/*This is the link connected to the plus sign on the home page, for adding a word */
