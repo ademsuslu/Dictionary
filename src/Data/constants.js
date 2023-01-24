297d5d4f5a19a9488d1b688d0d044f75ab10515f
Adem suslu <84082893+ademsuslu@users.noreply.github.com>
2023-01-24 20:05:57 +0300

    Update constants.js

--- src/Data/constants.js
+++ src/Data/constants.js
@@ -3,18 +3,14 @@ export const telegramChannelName = 'opendictionary1'
 export const createWordButtonLink = 'https://t.me/opendictionary1/4`'
 export const ContributeLink =
   'https://github.com/ademsuslu/Dictionary/blob/main/src/Data/dictionary.js'
-
+export const HomePage = '/'
+export const AboutPage = '/About'
+export const Telegram = `https://t.me/${telegramChannelName}`
 //Navbar End
 
 // Toast Error Dynamic Message
 export const Message = `No results. Please check spelling and read About Us. Happy Searching!`
 // Toast Error Dynamic Message  End
-
-// Like And Save BUttons Links
-import data from './dictionary'
-export const likeLink = `tg://resolve?domain=opendictionary1&post=${data?.telegramPost}`
-export const saveLink = `https://t.me/${telegramChannelName}/${data?.telegramPost}`
-// Like And Save BUttons Links end
 
 
 /*This is the telegram group name for the dictionary */
