95fd0afc8e01236f8d6fa43192bd41451bb19dfc
Adem suslu <84082893+ademsuslu@users.noreply.github.com>
2023-01-24 19:08:35 +0300

    Update constants.js

--- src/Data/constants.js
+++ src/Data/constants.js
@@ -1,2 +1,9 @@
-export const telegramChannelName = "opendictionary1"; /*This is the telegram group name for the dictionary */
+// For you Navbar in Links Dynamic Changed
+export const createWordButtonLink = 'https://t.me/opendictionary1/4`'
+export const ContributeLink =
+  'https://github.com/ademsuslu/Dictionary/blob/main/src/Data/dictionary.js'
+// Navbar End
+
+
+/*This is the telegram group name for the dictionary */
 /*This is the link connected to the plus sign on the home page, for adding a word */
