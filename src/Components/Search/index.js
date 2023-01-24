4feefe807bdbff6b83eee7a556cdd177e8d601b3
Adem suslu <84082893+ademsuslu@users.noreply.github.com>
2023-01-24 19:14:18 +0300

    Update index.js

--- src/Components/Search/index.js
+++ src/Components/Search/index.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react'
 import { useDebounce } from 'use-debounce'
 import { generatedData } from '@/pages/[slug]'
 import toast from 'react-hot-toast'
+import { Message } from '@/Data/constants'
 
 const searchByName = (obj, searchTerm) => {
   // Use Object.values to get an array of the values in the object
@@ -21,9 +22,7 @@ const searchByName = (obj, searchTerm) => {
   })
 
   if (results?.length === 0) {
-    toast.error(
-      `No results ${searchTerm}. Please check spelling and read About Us. Happy Searching!`
-    )
+    toast.error(Message)
   }
 
   return results
