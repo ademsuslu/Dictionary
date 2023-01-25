a12a5ce89206907d01c01579bd5585ad97b30044
Adem suslu <84082893+ademsuslu@users.noreply.github.com>
2023-01-25 18:16:59 +0300

    Update SearchItem.js

--- src/Components/Search/SearchItem.js
+++ src/Components/Search/SearchItem.js
@@ -1,21 +1,22 @@
-import Link from "next/link";
-import React from "react";
+import Link from 'next/link'
+import React from 'react'
 
 export default function SearchItem({ item }) {
   return (
-    <Link href={`/${item?.slug}`} className="DataItem">
-      <div className="nameVersion">
-        <h3 className="ItemName">{item?.name}</h3>
-        <p className="ItemVersion">{item?.wordVersion}</p>
+    <Link href={`/${item?.slug}`} className='DataItem'>
+      <div className='nameVersion'>
+        <h3 className='ItemName'>{item?.name}</h3>
+        <p className='ItemVersion'>{item?.wordVersion}</p>
       </div>
-      <div className="SearchItemDetay">
-        <span className="ItemDesc">{item?.description?.slice(0, 50)} </span>
-        ...
-        <p className="showMore" rel="noreferrer" href={`/${item?.slug}`}>
-          {" "}
-          More
-        </p>
+      <div className='SearchItemDetay'>
+        <span className='ItemDesc'>
+          {item?.description?.slice(0, 50)}
+          ...
+          <a className='showMore' rel='noreferrer' href={`/${item?.slug}`}>
+            More
+          </a>
+        </span>
       </div>
     </Link>
-  );
+  )
 }
